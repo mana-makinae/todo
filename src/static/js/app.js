2fabead2e30dd3e582028c99ba4c49f45e74028e
Mana Makinae <mana-ma@ws21is039.naist.jp>
2023-01-13 15:19:25 +0900

    change

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -51,6 +51,7 @@ function TodoListCard() {
 
     return (
         <React.Fragment>
+            <p className=“text-center”><font size=“4">ToDo App by Mana Makinae</font></p>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
                 <p className="text-center">No items yet! Add one above!</p>
