dd7b62e441a4492737f202b19626d2d840919d4b
Mana Makinae <107234251+mana-makinae@users.noreply.github.com>
2023-01-13 15:56:26 +0900

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -51,6 +51,7 @@ function TodoListCard() {
 
     return (
         <React.Fragment>
+            <p className=“text-center”><font size=“7">ToDo App by Yuta Hamada</font></p>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
                 <p className="text-center">No items yet! Add one above!</p>
